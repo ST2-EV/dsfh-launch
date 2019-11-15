88665b74754cc96af144aeab2e2dd7b4f9cefe4d
Steve Paul <steve.s.paul22@gmail.com>
2019-11-15 09:41:34 +0530

    build

---
+++ build/precache-manifest.8d72b24c31c026d4a03483a4205cdbde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e6267c158d9bee969f9fdce02df90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5eeba88dace694f4044",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b5e4fbdcefd438f3aeae",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a5eeba88dace694f4044",
+    "url": "/static/js/2.1f37c88f.chunk.js"
+  },
+  {
+    "revision": "b5e4fbdcefd438f3aeae",
+    "url": "/static/js/main.3555cf0a.chunk.js"
+  },
+  {
+    "revision": "28b5d344bab47312a225",
+    "url": "/static/js/runtime-main.c093e4b3.js"
+  }
+]);
